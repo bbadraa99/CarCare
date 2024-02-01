d13f5adfda90d918a6dc84745f8588fe7fc357d1
bbadraa99 <bbadraa8@gmail.com>
2024-02-01 14:59:10 +0800

    icon change more round and about section text adjustment

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='flexCenter flex-col bg-gray-100'>
+    <footer className='flexCenter flex-col bg-gray-100 padding-container'>
       <div className='header flexBetween flex-col sm:flex-row max-container 
          py-5 w-full center gap-5 sm:gap-10 lg:gap-20' style={{borderTop: '0.5px solid #c7c8c7'}}>
         <p className='bold-20 text-black'>Car Care</p>
@@ -23,7 +23,7 @@ const Footer = () => {
         </div>
         <div className='flex flex-row flexBetween gap-3'>
           <p className='regular-16 text-black'> Social:</p>
-          <div className='flexBetween flex-row'>
+          <div className='flexBetween flex-row '>
             <Link href="https://www.facebook.com/profile.php?id=61555200031184" target="_blank">
               <Image src="/facebook logo.svg" alt="" width={27} height={27}/>
             </Link>
